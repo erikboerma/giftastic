9f2c92d191640f005807a0663276a9199fa4919c
ErikTheDeveloper <ImErikTheDeveloper@gmail.com>
2019-09-18 20:14:29 -0400

    added css to box each image, also added on click for picture to animate (not working).

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,8 @@ var gifOptions = ["dog", "fish", "cat"];
 
 
 
+
+
 // create the button from the array 
 function createButtons() {
 
@@ -19,6 +21,8 @@ function createButtons() {
 
   // get api info with details for gif 
   $("button").on("click", function getGIF() {
+    $("#gifView").empty();
+
     var gifName = $(this).attr("data-name");
     console.log(gifName);
     // can change the number at the end of the url to control the amount of responses
@@ -27,29 +31,54 @@ function createButtons() {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-     
+    }).then(function (response) {
+      console.log(response);
 
       var results = response.data;
 
-      for (i=0; i < results.length; i++){
+      for (i = 0; i < results.length; i++) {
         // creates the div for the image and rating 
-        var resultsDiv=$("<div>");
+        var resultsDiv = $("<div id='results'>");
         // generates the rating 
         var p = $("<p>").text("Rating: " + results[i].rating);
         // this generates the image 
-        var image = $("<img>");
-        image.attr("src", results[i].images.preview_gif.url);
+        var image = $("<img class='gifPic' data-state='still'>");
+        image.attr("src", results[i].images.downsized_still.url);
 
-        resultsDiv.append(p);
         resultsDiv.append(image);
+        resultsDiv.append(p);
 
         $("#gifView").append(resultsDiv);
 
+
+        $(".gifPic").on("click", function () {
+          var state = $(this).attr("data-state");
+          console.log(state);
+          console.log(this);
+          if (state === "still") {
+            $(this).attr("src", results[i].images.downsized_medium.url);
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", results[i].images.downsized_still.url);
+            $(this).attr("data-state", "still");
+          }
+
+        })
       }
+
     })
   })
+
 };
+
+// $("#gifPic").on("click", function () {
+//   var state = $(this).attr("data-state");
+//   console.log(state);
+
+
+
+// url for gif that moves =====
+// image.attr("src", results[i].images.downsized_medium.url);
 
 
 // push any new option into the array and create a button 
@@ -61,6 +90,10 @@ $("#add-gif").on("click", function (event) {
   createButtons();
 
 
+
+
 });
+
+
 
 createButtons();
